4c2852aefaf4e7f4e73f0380db3317e61c298609
yrzgithub <85425005+yrzgithub@users.noreply.github.com>
2022-08-02 13:08:58 +0530

    Add files via upload

---
+++ code 2.py
@@ -0,0 +1,20 @@
+import keyboard
+import time
+
+msg = "Hello"
+num = 100000
+
+print("Waiting for shift..")
+keyboard.wait("shift")
+print("Press ctrl to stop...")
+
+while not keyboard.is_pressed("ctrl"):
+    to_print = f"{num}.{msg}"
+    keyboard.write(to_print)
+    time.sleep(0.1)
+    keyboard.press_and_release("enter")
+    time.sleep(0.1)
+    num+=1
+
+print("Loop stopped..")
+print("Messages sent...")
